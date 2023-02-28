461fd1796427fae0b43cc09ee53720f82b0c956f
Alex Fun <alexanderthefun91@gmail.com>
2023-02-28 14:03:12 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public class Company
+        {
+            public string Name { get; set; }
+        }
+
+
+        public class Employee
+        {
+            public string Name { get; set; }
+            public string Company { get; set; }
+            public string Title { get; set; }
+        }
+
+        // Assign the employees to the company
+
+        /*
+            Iterate the company's employee list and generate the
+            simple report shown above
+        */
+    }
+}
+}
+
+
+
+
+
+
+
